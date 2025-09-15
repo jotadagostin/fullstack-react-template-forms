bea9c8c817c580c09e7893ae0316f31275f5a92e
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-09-15 10:41:48 +0200

    finishing the form

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
 import "./App.css";
 import { Controller, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 type FormData = {
   name: string;
   date: string;
   subject: string;
+  description: string;
 };
 
+const schema = yup.object({
+  name: yup.string().required("Name is required!"),
+  date: yup.string().required("Date is required!"),
+  subject: yup.string().required("Select a subject!"),
+  description: yup
+    .string()
+    .required("Description is required!")
+    .min(10, "Description need at least 10 carachters"),
+});
+
 export default function App() {
-  const { control, handleSubmit } = useForm<FormData>({
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
     defaultValues: {
       name: "",
       date: "",
+      subject: "",
+      description: "",
     },
+    resolver: yupResolver(schema),
   });
 
   function onSubmit(data: FormData) {
@@ -31,7 +51,10 @@ export default function App() {
             <input type="text" placeholder="Name of the event" {...field} />
           )}
         />
-        <span className="error">Name is required</span>
+
+        {errors.name?.message && (
+          <span className="error">{errors.name.message}</span>
+        )}
 
         <Controller
           control={control}
@@ -40,6 +63,10 @@ export default function App() {
             <input type="date" placeholder="Name of the event" {...field} />
           )}
         />
+
+        {errors.date?.message && (
+          <span className="error">{errors.date.message}</span>
+        )}
 
         <Controller
           control={control}
@@ -58,7 +85,21 @@ export default function App() {
           )}
         />
 
-        <textarea placeholder="Descrição" rows={4} />
+        {errors.subject?.message && (
+          <span className="error">{errors.subject.message}</span>
+        )}
+
+        <Controller
+          control={control}
+          name="description"
+          render={({ field }) => (
+            <textarea placeholder="Description" rows={4} {...field} />
+          )}
+        />
+
+        {errors.description?.message && (
+          <span className="error">{errors.description.message}</span>
+        )}
 
         <button type="submit">Save</button>
       </form>
